5a59578c10fe343a758abf0f6e9d09b77692191f
WhyAsh5114 <whyash5114@gmail.com>
2024-10-01 17:54:00 +0530

    feat: Added validateRequest method

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,9 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
+import type { Session, User } from "lucia";
 import { Lucia } from "lucia";
+import { cookies } from "next/headers";
+import { cache } from "react";
 
 const client = new PrismaClient();
 const adapter = new PrismaAdapter(client.session, client.user);
@@ -18,6 +21,42 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+    } catch {}
+    return result;
+  }
+);
 
 declare module "lucia" {
   interface Register {
