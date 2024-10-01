007ecec6f5ad9c2585f924bf2fe79e3a78730a4f
WhyAsh5114 <whyash5114@gmail.com>
2024-10-01 23:08:43 +0530

    feat: Moved form to client component

--- app/profile/components/SkillsComponent.tsx
+++ app/profile/components/SkillsComponent.tsx
@@ -1,15 +1,61 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { X, Plus } from "lucide-react";
 import { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
-export async function SkillsComponent() {
+export function SkillsComponent() {
+  const [skillToAdd, setSkillToAdd] = useState("");
   const [skills, setSkills] = useState<string[]>(["React", "Python"]);
 
+  function addSkill() {
+    const skill = skillToAdd;
+    setSkillToAdd("");
+    if (skills.includes(skill)) {
+      return false;
+    }
+    setSkills([...skills, skill]);
+  }
+
+  function removeSkill(skill: string) {
+    setSkills(skills.filter((_skill) => _skill !== skill));
+  }
+
   return (
-    <div className="flex flex-wrap gap-1">
-      {skills.map((skill) => (
-        <Badge key={skill}>{skill}</Badge>
-      ))}
-    </div>
+    <>
+      <div className="flex items-end gap-1">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="skill-name">Search skills</Label>
+          <Input
+            id="skill-name"
+            value={skillToAdd}
+            onChange={(e) => setSkillToAdd(e.target.value)}
+          />
+        </div>
+        <Button variant="secondary" className="gap-2" onClick={addSkill}>
+          Add
+          <Plus className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className="flex flex-wrap gap-1">
+        {skills.map((skill) => (
+          <Badge
+            className="flex items-center justify-between gap-1"
+            key={skill}
+          >
+            {skill}
+            <Button
+              className="w-4 h-4 p-0"
+              variant="ghost"
+              onClick={() => removeSkill(skill)}
+            >
+              <X />
+            </Button>
+          </Badge>
+        ))}
+      </div>
+    </>
   );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,11 +10,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+
 import { Button } from "@/components/ui/button";
 import { SkillsComponent } from "./components/SkillsComponent";
-import { Plus } from "lucide-react";
 
 export default async function Page() {
   return (
@@ -29,16 +27,6 @@ export default async function Page() {
           <CardDescription>Manage your skills here</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-2">
-          <div className="flex items-end gap-1">
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Label htmlFor="skill-name">Search skills</Label>
-              <Input id="skill-name" placeholder="Type here" />
-            </div>
-            <Button variant="secondary" className="gap-2">
-              Add
-              <Plus className="w-4 h-4" />
-            </Button>
-          </div>
           <SkillsComponent />
         </CardContent>
         <CardFooter>
