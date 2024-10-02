aa4ec0d9b3189c2a1e1370bb26081e88d1bcead7
WhyAsh5114 <whyash5114@gmail.com>
2024-10-02 11:10:13 +0530

    feat: Added a profile changes section with profile picture component

---
+++ app/profile/components/ProfilePictureComponent.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getInitials } from "@/lib/utils";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
+
+export default function ProfilePictureComponent() {
+  const [username, setUsername] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+
+  return (
+    <div className="grid grid-cols-2 place-items-center gap-y-6">
+      <Avatar className="h-16 w-16 items-center justify-center bg-muted">
+        {profilePicture === "" ? (
+          <User className="h-10 w-10" />
+        ) : (
+          <AvatarImage src={profilePicture} />
+        )}
+      </Avatar>
+      <Avatar className="h-16 w-16">
+        <AvatarFallback>{getInitials(username)}</AvatarFallback>
+      </Avatar>
+      <div className="grid w-full items-center gap-1.5 col-span-2">
+        <Label htmlFor="username">Username</Label>
+        <Input id="username" placeholder="Type here" minLength={2} />
+      </div>
+      <div className="grid w-full items-center gap-1.5 col-span-2">
+        <Label htmlFor="profile-picture">Profile picture</Label>
+        <Input id="profile-picture" placeholder="Paste a link" />
+      </div>
+      <Button className="col-span-2 ml-auto">Save changes</Button>
+    </div>
+  );
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,11 +3,13 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle
+  CardTitle,
 } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TypographyH2 } from "@/components/ui/typographyH2";
 import { Suspense } from "react";
+import ProfilePictureComponent from "./components/ProfilePictureComponent";
 import { SkillsDataWrapper } from "./components/SkillsDataWrapper";
 import { UsernameComponent } from "./components/UsernameComponent";
 
@@ -18,17 +20,38 @@ export default async function Page() {
       <Suspense fallback={<Skeleton className="h-6 mt-2 w-32" />}>
         <UsernameComponent />
       </Suspense>
-      <Card className="rounded-lg mt-4">
-        <CardHeader>
-          <CardTitle>Skills</CardTitle>
-          <CardDescription>Manage your skills here</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <Suspense fallback={<Skeleton className="h-36 w-full" />}>
-            <SkillsDataWrapper />
-          </Suspense>
-        </CardContent>
-      </Card>
+      <Tabs defaultValue="profile" className="w-full mt-4">
+        <TabsList className="w-full grid grid-cols-3">
+          <TabsTrigger value="profile">Profile</TabsTrigger>
+          <TabsTrigger value="skills">Skills</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">
+          <Card className="rounded-lg">
+            <CardHeader>
+              <CardTitle>Profile</CardTitle>
+              <CardDescription>
+                Make changes to your profile here
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <ProfilePictureComponent />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="skills">
+          <Card className="rounded-lg">
+            <CardHeader>
+              <CardTitle>Skills</CardTitle>
+              <CardDescription>Manage your skills here</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <Suspense fallback={<Skeleton className="h-36 w-full" />}>
+                <SkillsDataWrapper />
+              </Suspense>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
