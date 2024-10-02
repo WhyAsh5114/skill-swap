9921397876068978cd9c8ff31c74faf418ed6945
WhyAsh5114 <whyash5114@gmail.com>
2024-10-02 18:47:12 +0530

    feat: Auto scrolling and enter-based message sending

--- app/chat/[withUserId]/components/ChatWindow.tsx
+++ app/chat/[withUserId]/components/ChatWindow.tsx
@@ -2,10 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { sendMessageAction } from "./actions";
 import type { Message } from "@prisma/client";
 import { Badge } from "@/components/ui/badge";
+import { LoaderCircle, SendHorizontal } from "lucide-react";
 
 type PropsType = {
   toUserId: string;
@@ -15,10 +16,14 @@ type PropsType = {
 export default function ChatWindow({ toUserId, allMessages }: PropsType) {
   const [messages, setMessages] = useState<Message[]>(allMessages);
   const [message, setMessage] = useState("");
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const [sending, setSending] = useState(false);
 
   async function sendMessage() {
+    setSending(true);
     const sentMessage = await sendMessageAction(message, toUserId);
     setMessage("");
+    setSending(false);
     setMessages([...messages, sentMessage]);
   }
 
@@ -32,6 +37,7 @@ export default function ChatWindow({ toUserId, allMessages }: PropsType) {
         );
         const result = await response.json();
         setMessages([...messages, ...result]);
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -45,31 +51,45 @@ export default function ChatWindow({ toUserId, allMessages }: PropsType) {
 
   return (
     <>
-      <div className="flex h-px grow flex-col overflow-y-auto my-4 gap-1.5">
+      <div className="flex h-px grow flex-col overflow-y-auto my-4 gap-2">
         {messages.map((message, idx) => {
           if (message.fromUserId === toUserId) {
             return (
-              <Badge key={idx} className="mr-auto max-w-64" variant="outline">
+              <Badge
+                key={idx}
+                className="mr-auto max-w-64 text-sm font-medium"
+                variant="outline"
+              >
                 {message.text}
               </Badge>
             );
           } else {
             return (
-              <Badge key={idx} className="ml-auto max-w-64">
+              <Badge key={idx} className="ml-auto max-w-64 text-sm font-medium">
                 {message.text}
               </Badge>
             );
           }
         })}
+        <div ref={bottomRef} />
       </div>
-      <div className="flex gap-1">
+      <form
+        className="flex gap-1"
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage();
+        }}
+      >
         <Input
           placeholder="Type here"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <Button onClick={sendMessage}>Send</Button>
-      </div>
+        <Button type="submit" className="gap-2" disabled={sending}>
+          {sending ? <LoaderCircle className="animate-spin h-6 w-6" /> : "Send"}
+          <SendHorizontal className="h-4 w-4" />
+        </Button>
+      </form>
     </>
   );
 }
