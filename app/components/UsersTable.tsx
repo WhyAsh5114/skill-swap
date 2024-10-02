f7dc26aa06f8d95fd271b7fa06b08f33b13157c9
WhyAsh5114 <whyash5114@gmail.com>
2024-10-02 13:46:55 +0530

    feat: Added wanted skills to userTable

--- app/components/UsersTable.tsx
+++ app/components/UsersTable.tsx
@@ -17,7 +17,7 @@ export async function UsersTable() {
   const { user } = await validateRequest();
   if (!user) return redirect("/login");
   const users = await prisma.user.findMany({
-    include: { knownSkills: true },
+    include: { knownSkills: true, wantedSkills: true },
     where: { id: { not: user.id } },
   });
 
@@ -26,7 +26,8 @@ export async function UsersTable() {
       <TableHeader>
         <TableRow>
           <TableHead colSpan={2}>User</TableHead>
-          <TableHead className="w-40">Skills</TableHead>
+          <TableHead className="w-28">Known</TableHead>
+          <TableHead className="w-28">Wanted</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -39,15 +40,29 @@ export async function UsersTable() {
               </Avatar>
             </TableCell>
             <TableCell>{user.username}</TableCell>
-            <TableCell className="flex flex-wrap gap-1">
-              {user.knownSkills.map((skill) => (
-                <Badge key={skill.skillName}>{skill.skillName}</Badge>
-              ))}
-              {user.knownSkills.length === 0 ? (
-                <Badge variant="outline">None</Badge>
-              ) : (
-                <></>
-              )}
+            <TableCell>
+              <div className="flex flex-wrap gap-1">
+                {user.knownSkills.map((skill) => (
+                  <Badge key={skill.skillName}>{skill.skillName}</Badge>
+                ))}
+                {user.knownSkills.length === 0 ? (
+                  <Badge variant="outline">None</Badge>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </TableCell>
+            <TableCell>
+              <div className="flex flex-wrap gap-1">
+                {user.wantedSkills.map((skill) => (
+                  <Badge key={skill.skillName}>{skill.skillName}</Badge>
+                ))}
+                {user.wantedSkills.length === 0 ? (
+                  <Badge variant="outline">None</Badge>
+                ) : (
+                  <></>
+                )}
+              </div>
             </TableCell>
           </TableRow>
         ))}
