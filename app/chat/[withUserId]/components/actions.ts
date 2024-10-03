1be32c325cda7d3daab53c5682a12d30f490d13d
WhyAsh5114 <whyash5114@gmail.com>
2024-10-03 15:25:57 +0530

    feat: Send file props if provided

--- app/chat/[withUserId]/components/actions.ts
+++ app/chat/[withUserId]/components/actions.ts
@@ -2,22 +2,32 @@
 
 import { validateRequest } from "@/lib/auth";
 import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-export async function sendMessageAction(message: string, toUserId: string) {
+export async function sendMessageAction(
+  message: string,
+  toUserId: string,
+  file?: File
+) {
   const { user } = await validateRequest();
   if (!user) return redirect("/login");
 
-  const messageObject = await prisma.message.create({
-    data: {
-      createdAt: new Date(),
-      text: message,
-      fromUserId: user.id,
-      toUserId,
-    },
-  });
+  const createData: Prisma.MessageUncheckedCreateInput = {
+    createdAt: new Date(),
+    text: message,
+    fromUserId: user.id,
+    toUserId,
+  };
 
-  return messageObject;
+  if (file) {
+    createData.filename = file.name;
+    createData.file = Buffer.from(await file.arrayBuffer());
+  }
+
+  await prisma.message.create({
+    data: createData,
+  });
 }
 
 export async function getNewMessages(
@@ -35,6 +45,7 @@ export async function getNewMessages(
       ],
       createdAt: { gt: new Date(lastMessageAt) },
     },
+    omit: { file: true },
     orderBy: { createdAt: "asc" },
   });
 }
